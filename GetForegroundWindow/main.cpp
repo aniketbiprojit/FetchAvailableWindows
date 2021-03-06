fdc4a6ade0c2f8701e7ac5c56ade4317a43cf93d
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-03-06 21:56:55 +0530

    feat: some changes

--- GetForegroundWindow/main.cpp
+++ GetForegroundWindow/main.cpp
@@ -21,18 +21,44 @@ int main()
 	HWND hWnd;
 	hWnd = GetForegroundWindow();
 	std::vector<WindowDataClass> windowTitles;
+<<<<<<< HEAD
 	
 	getProcessInfo(hWnd, windowTitles, true);
 		
 	
 	
 	for (size_t i = 0; i < windowTitles.size(); i++)
+=======
+	std::cout << hWnd;
+	getProcessInfo(hWnd, windowTitles);
+	while (true)
+>>>>>>> 1f97f82 (feat: some changes)
 	{
-		auto elem = windowTitles[i];
-		elem.print_yaml();
+		for (size_t i = 0; i < windowTitles.size(); i++)
+		{
+			auto elem = windowTitles[i];
+			elem.print_yaml();
+		}
+		Sleep(2000);
 	}
+<<<<<<< HEAD
 	
 		
 	CoUninitialize();
      /*std::cout << "Hello World!\n";*/
 }
+=======
+	// std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+>>>>>>> 1f97f82 (feat: some changes)
